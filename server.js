d3ff0561c6aa14087f81b21e96eb7d0d9b17f963
Arnj121 <arnj121@gmail.com>
2024-10-16 22:22:10 -0500

    ..

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ server.use('/public',express.static(path.join(__dirname,'static')))
 server.use('/',express.static(path.join(__dirname,'static')))
 server.get('/test',(req,res)=>{
 	res.send({'msg':'recieving'})})
-server.listen(process.env.PORT,()=>{
-    console.log('serving font awesome icons')
+server.listen(process.env.PORT,process.env.HOST,()=>{
+    console.log(`serving font awesome icons on http://${process.env.HOST}:${process.env.PORT}/`)
 })
