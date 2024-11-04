0da30ae19dbac3999b2e3ab4c1ed26bd80ff2b8c
Arnj121 <arnj121@gmail.com>
2024-11-04 15:36:33 -0600

    initip

--- initIP.js
+++ initIP.js
@@ -2,26 +2,23 @@ const fs = require('fs')
 const dns = require('node:dns')
 const os = require('node:os')
 require('dotenv').config()
-const ip1=process.env.IP
 const ip2=process.argv[2]
 const newip=process.argv[3]
-let files=['static/main/index.html','.env','static/main/script.js','JSON/apps.json','create-app.js']
-dns.lookup(os.hostname(),{family:4},(err,addr)=>{
-    for(let i=0;i<files.length;i++) {
-        fs.readFile(files[i], 'UTF-8', (err, data) => {
-            if (err) {
-                console.log(err)
-            } else {
-                let loc = data.indexOf(ip1)
-                while (loc != -1) {
-                    data = data.replace(ip1, addr)
-                    loc = data.indexOf(ip1)
-                    console.log(loc)
-                }
-                console.log(data)
-                fs.writeFile(files[i], data, () => {
-                })
+let files=['static/fontawesome.css','.env']
+for(let i=0;i<files.length;i++) {
+    fs.readFile(files[i], 'UTF-8', (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            let loc = data.indexOf(ip2)
+            while (loc != -1) {
+                data = data.replace(ip2, newip)
+                loc = data.indexOf(ip2)
+                console.log(loc)
             }
-        })
-    }
-})
+            console.log(data)
+            fs.writeFile(files[i], data, () => {
+            })
+        }
+    })
+}
